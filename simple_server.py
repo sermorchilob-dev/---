6568a2089236bca92b7f41ec6d562111cd25dbb2
Валентин <ser.morchilob@gmail.com>
2026-06-13 21:08:13 +0000

    Добавлен Procfile, адаптация simple_server.py под PORT

--- simple_server.py
+++ simple_server.py
@@ -514,4 +514,5 @@ def get_selection_result(session_id: str, db: Session = Depends(get_db)):
 # ---------------------- MAIN ----------------------
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
